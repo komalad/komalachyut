2014881115935c7721c27da9f629dfa334b5dc30
Achyut Devkota <achyut_devkota@gap.com>
2021-11-09 12:25:03 -0500

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+function writeUserData(userName, attending) {
+    
+  }
+
+  $('#submit-button').on('click', function(event) {
+    console.log("Started");
+    event.preventDefault(); 
+    var fullName =$("fullNameInput");
+    firebase.database().ref('guest/' + fullName).set({
+        fullName : fullName,
+        attending: "Y"
+      });
+    console.log("done");
+  });
